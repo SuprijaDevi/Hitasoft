148bd9b051c22e7870b28fa9ce8552cbae0f73e0
SuprijaDevi <ropotther@gmail.com>
2024-07-19 17:47:08 +0530

    10

--- Day 10/sqlConnect.php
+++ Day 10/sqlConnect.php
@@ -25,13 +25,41 @@
         echo "Error creating table: " . $conn->error;
     }
 
+    //Insert values
     $insertsql = "INSERT INTO product (prodName, prodDescription, prodCount, sellerName)
     VALUES ('Dell', 'pad', 12, 'Ron')";
 
     if($conn->query($insertsql) == TRUE) {
-        echo "New record created";
+        echo "New record created <br>";
     } else {
         echo "Error: " . $insertsql . "<br>" . $conn->error;
+    }
+
+    //Read values
+    $selectsql = "SELECT prodName, prodDescription, prodCount, sellerName FROM product";
+    $result = $conn->query($selectsql);
+
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo "Product name: " . $row["prodName"] . ", " .
+            "Description: " . $row["prodDescription"] . ", " .
+            "Count: " . $row["prodCount"] . ", " .
+            "Seller name: " . $row["sellerName"] . "<br>";
+        }
+    } else {
+        echo "0 results.";
+    }
+
+    //Ubdate values
+    $updatesql = "UPDATE product SET prodCount = 13 WHERE prodName = 'Dell'";
+    if($conn->query($updatesql) != TRUE) {
+        echo "Error updating record: " . $conn->error;
+    }
+
+    //Delete values
+    $deletesql = "DELETE FROM product WHERE prodName = 'Dell'";
+    if($conn->query($deletesql) != TRUE) {
+        echo "Error deleting the value: " . $conn->error;
     }
 
     $conn->close();
